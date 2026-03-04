e44a12d594160a6a954f910c4bdc2a346c58b4d0
KGuseva <marzia240015@gmail.com>
2026-03-04 14:55:38 +0300

    fix: code style in test files

--- internal/domain/care_event_test.go
+++ internal/domain/care_event_test.go
@@ -1,14 +1,15 @@
 package domain
 
 import (
+	"errors"
 	"testing"
 	"time"
 )
 
 func TestCareEvent_EmptyPlantID(t *testing.T) {
-	_, err := NewCareEvent(0, "water", time.Now())
+	_, err := NewCareEvent(0, CareKindWater, time.Now())
 
-	if err == nil {
+	if !errors.Is(err, ErrInvalidArgument) {
 		t.Error("Plant ID can't be empty or negative number")
 	}
 }
@@ -16,16 +17,22 @@ func TestCareEvent_EmptyPlantID(t *testing.T) {
 func TestCareEvent_InvalidKind(t *testing.T) {
 	_, err := NewCareEvent(10, "freeze", time.Now())
 
-	if err == nil {
+	if !errors.Is(err, ErrInvalidArgument) {
 		t.Errorf("Plant kind can't be %q, allowed only 'water' and 'fertilize'", "freeze")
 	}
 }
 
 func TestCareEvent_InvalidTime(t *testing.T) {
-	var occurredAt time.Time
-	_, err := NewCareEvent(10, "fertilize", occurredAt)
+	loc := time.FixedZone("MSK", 3*60*60)         // UTC+3
+	in := time.Date(2026, 3, 4, 12, 0, 0, 0, loc) // 12:00 MSK
 
-	if err == nil {
-		t.Errorf("Occurred time can't be empty")
+	ev, err := NewCareEvent(10, CareKindFertilize, in)
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if ev.OccurredAt.Location() != time.UTC {
+		t.Fatalf("OccurredAt must be UTC, got %v", ev.OccurredAt.Location())
 	}
 }
--- internal/domain/plant_test.go
+++ internal/domain/plant_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestPlant_EmptyUserID(t *testing.T) {
@@ -17,7 +18,7 @@ func TestPlant_EmptyUserID(t *testing.T) {
 func TestPlant_EmptyName(t *testing.T) {
 	_, err := NewPlant(10, "")
 
-	if err == nil {
+	if !errors.Is(err, ErrInvalidArgument) {
 		t.Error("Plant name can't be empty")
 	}
 }
@@ -25,7 +26,11 @@ func TestPlant_EmptyName(t *testing.T) {
 func TestPlant_TrimName(t *testing.T) {
 	plantName := " Cactus Poppy  "
 
-	plant, _ := NewPlant(10, plantName)
+	plant, err := NewPlant(10, plantName)
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
 
 	if plant.Name != strings.TrimSpace(plantName) {
 		t.Error("Plant name don't trim")
@@ -33,9 +38,17 @@ func TestPlant_TrimName(t *testing.T) {
 }
 
 func TestPlant_InvalidCreateDate(t *testing.T) {
-	plant, _ := NewPlant(10, "cactus")
+	plant, err := NewPlant(10, "cactus")
+
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
 
 	if plant.CreatedAt.IsZero() {
 		t.Error("Plant created date can't be empty")
+	}
+
+	if plant.CreatedAt.Location() != time.UTC {
+		t.Fatalf("CreatedAt must be UTC, got %v", plant.CreatedAt.Location())
 	}
 }
